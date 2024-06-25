a9f07b4e18f6d0f46fe2406eff9a4581b3b1f441
NGUYEN MINH THONG <thongnm@lgcns.com>
2024-06-25 03:01:30 -0700

    Implement JPA

--- src/main/java/com/tommy/coding/jpa/JpaApplication.java
+++ src/main/java/com/tommy/coding/jpa/JpaApplication.java
@@ -1,5 +1,7 @@
 package com.tommy.coding.jpa;
 
+import com.github.javafaker.Faker;
+import com.tommy.coding.jpa.models.Author;
 import com.tommy.coding.jpa.models.Video;
 import com.tommy.coding.jpa.repositories.AuthorRepository;
 import com.tommy.coding.jpa.repositories.VideoRepository;
@@ -7,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -21,11 +26,26 @@ public class JpaApplication {
 			VideoRepository videoRepository
 	){
 		return args -> {
-			var video = Video.builder()
-					.name("Tommy")
-					.length(100)
-					.build();
-			videoRepository.save(video);
+//			Faker faker = new Faker();
+//			for (int i = 0; i < 50; i++) {
+//				var author = Author.builder()
+//						.firstName(faker.name().firstName())
+//						.lastName(faker.name().lastName())
+//						.email("contact" + i + "@gmail.com")
+//						.build();
+
+//				authorRepository.save(author);
+//			}
+
+			List<Author> authors = authorRepository.findAllByFirstNameStartsWith("P");
+			System.out.println(authors);
+
+
+//			var video = Video.builder()
+//					.name("Tommy")
+//					.length(100)
+//					.build();
+//			videoRepository.save(video);
 		};
 	}
 }
--- src/main/java/com/tommy/coding/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/tommy/coding/jpa/repositories/AuthorRepository.java
@@ -3,6 +3,8 @@ package com.tommy.coding.jpa.repositories;
 import com.tommy.coding.jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuthorRepository  extends JpaRepository<Author, Integer> {
+import java.util.List;
 
+public interface AuthorRepository  extends JpaRepository<Author, Integer> {
+    List<Author> findAllByFirstNameStartsWith(String fn);
 }
